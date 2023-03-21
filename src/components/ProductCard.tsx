a344b708e9e041abaa2e86342728a76a92081b07
betterplaywon <kkgi1234@gmail.com>
2023-03-22 00:05:17 +0900

    add: ProductCard 클릭 시 디테일 페이지로 이동 기능 추가

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const ProductCard = ({ product }) => {
+  const router = useRouter();
   const { title, price, id, description, category, image } = product;
+
+  const routeToDetail = () => {
+    router.push({ pathname: `/product/${id}`, query: product });
+  };
+
   return (
-    <li className="cursor-pointer overflow-hidden ">
+    <li className="cursor-pointer overflow-hidden" onClick={routeToDetail}>
       <img className="" src={image} />
       <div className="flex flex-col items-center">
         <p className="text-sm mt-3 ">{title}</p>
-        <p className="text-sm">KRW {price}</p>
+        <p className="text-sm line-through">KRW {price}</p>
         <p className="mb-2 px-2 text-gray-600">{category}</p>
       </div>
     </li>
