04713c1d7c5b1fb0f78fdcc8362ebeb079b1fef1
betterplaywon <kkgi1234@gmail.com>
2023-03-23 14:46:38 +0900

    add: productDetail 페이지 생성

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "@/components/Nav";
+import { useRouter } from "next/router";
+import Button from "@/components/Button";
+import { useUserContext } from "@/context/UserContext";
+import { updateOrAddBag } from "../api/firebase";
+
+interface uidType {
+  uid?: string;
+}
+interface ProductDetailType {
+  category?: string;
+  description?: string;
+  image?: string;
+  options?: string;
+  price?: string;
+  title?: string;
+  id?: string;
+}
 
 const ProductDetail = () => {
+  const { uid }: uidType = useUserContext();
+
+  const router = useRouter();
+  const query = router.query;
+  const {
+    category,
+    description,
+    image,
+    options,
+    price,
+    title,
+    id,
+  }: ProductDetailType = query;
+  console.log(options);
+  const [selectOption, setSelectOption] = useState(options ?? [0]);
+
+  const handleSelect = e => {
+    const { value } = e.target;
+    setSelectOption(value);
+  };
+
+  const handleMyBag = () => {
+    const productInfo = {
+      image,
+      option: selectOption,
+      price,
+      title,
+      id,
+      quantity: 1,
+    };
+    updateOrAddBag(uid || "", productInfo);
+    alert("쇼핑백에 저장되었습니다");
+  };
+
   return (
     <>
       <Nav />
-      상품 세부정보
+      <p className="mt- 3 text-gray-600 text-lg">{category}</p>
+      <section className="flex flex-col md:flex-row justify-center">
+        <img
+          className="w-96 mx-auto mb-2"
+          src={image}
+          alt={title}
+          width="300"
+          height="300"
+        />
+        <article className="flex flex-col">
+          <h2 className="font-medium text-xl">{title}</h2>
+          <p className="font-bold text-sm line-through">KRW {price}</p>
+          <p className="my-1 text-sm">detail: {description}</p>
+          <label className="text-sm" htmlFor="select">
+            Size
+          </label>
+          <select
+            id="select"
+            className="my-1"
+            onChange={handleSelect}
+            value={selectOption}
+          >
+            {options?.map((opt, idx) => (
+              <option key={idx}>{opt}</option>
+            ))}
+          </select>
+          <Button buttonText="BUY" onClick={handleMyBag} disabled={false} />
+        </article>
+      </section>
     </>
   );
 };
