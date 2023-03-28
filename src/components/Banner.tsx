af75b86bb9a60c5d8f50d0c773f1cdd24e2473e3
betterplaywon <kkgi1234@gmail.com>
2023-03-28 16:02:11 +0900

    fix: banner video로 수정 & public에 video file 추가

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <section className="h-96 bg-yellow-900 relative">
+    <section className="h-96 bg-black relative">
       <div className="w-full h-full bg-cover bg-banner">
-        <div className="absolute w-full top-32 text-center">
-          <h2 className="text-6xl">shop US</h2>
-          <p className="text-2xl">YES MENUAL</p>
-        </div>
+        <video className="w-full h-full" autoPlay muted loop>
+          <source src="/videos/nomanual_banner_video.mp4" type="video/mp4" />
+        </video>
       </div>
     </section>
   );
